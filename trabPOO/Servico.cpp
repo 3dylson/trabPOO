77e20961aab119764e5cbab717eacc31ab503d07
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-05-23 16:40:03 +0100

    Constructor, getters and setters

--- trabPOO/Servico.cpp
+++ trabPOO/Servico.cpp
@@ -1,1 +1,37 @@
 ﻿#include "Servico.h"
+
+Servico::Servico(const int id, const string& data, const float custo)
+	: id(id),
+	data(data),
+	custo(custo)
+{}
+
+int Servico::get_id() const
+{
+	return id;
+}
+
+void Servico::set_id(int id)
+{
+	this->id = id;
+}
+
+string Servico::get_data() const
+{
+	return data;
+}
+
+void Servico::set_data(const string& data)
+{
+	this->data = data;
+}
+
+float Servico::get_custo() const
+{
+	return custo;
+}
+
+void Servico::set_custo(float custo)
+{
+	this->custo = custo;
+}
--- trabPOO/Servico.h
+++ trabPOO/Servico.h
@@ -1,7 +1,25 @@
 ﻿#pragma once
+#include <string>
+
+using namespace std;
 
 class Servico
 {
+protected:
+	int id;
+	string data;
+	float custo;
+
 public:
-	
+
+	Servico(const int id, const string& data, const float custo);
+
+	//Getters e Setters
+	int get_id() const;
+	void set_id(int id);
+	string get_data() const;
+	void set_data(const string& data);
+	float get_custo() const;
+	void set_custo(float custo);
+
 };
