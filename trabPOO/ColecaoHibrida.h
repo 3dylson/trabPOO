223f2f708f790ed27d34c4759218efc911a0740f
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-05-28 20:55:27 +0100

    update version

--- trabPOO/ColecaoHibrida.h
+++ trabPOO/ColecaoHibrida.h
@@ -1,27 +1,26 @@
-///////////////ColecaoHibrida.h (vrs. 13/Dez/2018)////////////////
+///////////////ColecaoHibrida.h (vrs. 27/maio/2021)////////////////
 #pragma once
 #include<set>
 using namespace std;
 
-
 template<class T>
 class less_pointers
-{	
+{
 public:
-	bool operator()(const T &left, const T &right) const
+	bool operator()(const T& left, const T& right) const
 	{
 		return (*left < *right);
 	}
 };
 
 template<class K>
-class ColecaoHibrida: public set<K, less_pointers<K>>
+class ColecaoHibrida : public set<K, less_pointers<K>>
 {
 public:
-	bool insert(const K &c);
-	K find(const K &c) const;
+	bool insert(const K& c);
+	K find(const K& c) const;
 	int size() const;
-	void erase(const K &);
+	void erase(const K&);
 	//void clear();
 	//bool empty() const;
 	//iterator begin();
@@ -29,28 +28,28 @@ public:
 };
 
 template<class K>
-bool ColecaoHibrida<K>::insert(const K &c)
+bool ColecaoHibrida<K>::insert(const K& c)
 {
-	pair<set<K, less_pointers<K>>::iterator, bool> r=set<K, less_pointers<K>>::insert(c);
+	pair<typename set<K, less_pointers<K>>::iterator, bool> r = set<K, less_pointers<K>>::insert(c);
 	return(r.second);
 }
 
 template<class K>
-K ColecaoHibrida<K>::find(const K &c) const
-{	
-	K r=0;
-	typename set<K, less_pointers<K>>::iterator i=set<K, less_pointers<K>>::find(c);
-	if(i!=set<K, less_pointers<K>>::end())
-		r=*i;		
+K ColecaoHibrida<K>::find(const K& c) const
+{
+	K r = 0;
+	typename set<K, less_pointers<K>>::iterator i = set<K, less_pointers<K>>::find(c);
+	if (i != set<K, less_pointers<K>>::end())
+		r = *i;
 	return(r);
 }
 
 template<class K>
-int ColecaoHibrida<K>::size() const{	
+int ColecaoHibrida<K>::size() const {
 	return((int)set<K, less_pointers<K>>::size());
 }
 
 template<class K>
-void ColecaoHibrida<K>::erase(const K &c ) {	
+void ColecaoHibrida<K>::erase(const K& c) {
 	set<K, less_pointers<K>>::erase(c);
 }
